7d1bb65e4aa9ad24a76791db4b0d0230aaaf4d14
Mauricio Rojas <mrojasb2000@gmail.com>
2024-09-10 21:08:59 -0300

    chore: add basic movie details

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
 import './App.css'
+import responseMovies from './mocks/with-results.json'
+import withoutResults from './mocks/no-results.json'
+
 
 function App() {
+  const movies = responseMovies.Search
+  const hasMovies = movies?.length > 0
 
   return (
-   <h1>Hello World!</h1>
+   <div className='page'>
+    <header>
+     <h1>Movie Search</h1>
+     <form className='form'>
+       <input type='text' placeholder='Advengers, Star Ward, The Matrix...' />
+       <button type='submit'>Search</button>
+     </form>
+   </header>
+   <main>
+    {
+      hasMovies ? (
+        <ul>
+          {
+            movies.map(movie => (
+              <li key={movie.imdbID}>
+                <h3>{movie.Title}</h3>
+                <p>{movie.Year}</p>
+                <img src={movie.Poster} alt={movie.Title}/>
+              </li>
+            ))
+          }
+        </ul>
+      ) : (
+        <p>Not found</p>
+      )
+    } 
+   </main>
+   </div>
   )
 }
 
